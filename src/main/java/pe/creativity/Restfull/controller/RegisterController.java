26383428349a7349d7763fcdce01e5a66e36dbb8
Hansel <hansel.hir@gmail.com>
2020-07-06 23:56:16 -0500

    feat: Arreglando test y agregando ambientes en properties

--- src/main/java/pe/creativity/Restfull/controller/RegisterController.java
+++ src/main/java/pe/creativity/Restfull/controller/RegisterController.java
@@ -5,6 +5,8 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 import pe.creativity.Restfull.entity.Person;
 import pe.creativity.Restfull.service.PersonService;
@@ -15,6 +17,8 @@ import javax.validation.Valid;
 @RequestMapping("/v2")
 public class RegisterController {
 
+    Logger logger = LogManager.getLogger(RegisterController.class);
+
     @Autowired
     PersonService personService;
 
@@ -22,6 +26,16 @@ public class RegisterController {
     @ApiResponses(value = {@ApiResponse(code = 200, message = "OK")})
     @PostMapping("/RegistrarCliente")
     public Person addPerson(@RequestBody @Valid Person person) {
+        logger.info("Ingreso metodo endpoint  -- INICIO Creando un nuevo usuario");
+        logger.debug("Get Person Id: {}", person.getId());
+        logger.debug("Get FirstName: {}", person.getFirstName());
+        logger.debug("Get LastName: {}", person.getLastName());
+        logger.debug("Get Phone: {}", person.getPhone());
+        logger.debug("Get Email: {}", person.getEmail());
+        logger.debug("Get DocType: {}", person.getDocType());
+        logger.debug("Get DocNumber: {}", person.getDocNumber());
+        logger.debug("Get To Transfer: {}", person.getIsAbleToTransfer());
+        logger.debug("Get Country Code: {}", person.getCountryCode());
         return personService.savePerson(person);
     }
 
